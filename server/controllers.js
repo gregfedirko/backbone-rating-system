c8067092ba9d44d82c5f4b24f2a3632c6e98ed4f
Greg Fedirko <gjfedirko@gmail.com>
2015-01-24 23:24:40 -0800

    api endpoint for single restaraount

--- server/controllers.js
+++ server/controllers.js
@@ -7,3 +7,10 @@ exports.getRestaraunts = function(req, res) {
     res.json(collection);
   });
 };
+
+
+exports.getRestaraunt = function(req, res) {
+  Restaraunt.findOne({_id: req.params.id}, function(error, item) {
+    res.json(item);
+  });
+};
--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
 
   // Restaraunts
   app.get('/api/restaraunts', controllers.getRestaraunts);
+  app.get('/api/restaraunts/:id', controllers.getRestaraunt);
 
 
   // Serve up index.html as a default 
